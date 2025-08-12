4f595446c77a1967bfd1e773a1da482e388dab08
Shehab Ehab <shehabeldenehab@gmail.com>
2025-08-12 19:35:03 +0300

    create the method that returns the fie extension and edit on the list

--- file_organizer.py
+++ file_organizer.py
@@ -1,3 +1,10 @@
-imagesExtensions = [".jpg",".jpeg",".png", ".gif", ".bmp", ".tiff", ".svg", ".webp", ]
-videoExtensions = [".mp4",".mkv",".mov",".avi", ".flv",".wmv"]
-docsExtensions = [".pdf",".doc", ".docx",".xls", ".xlsx",".ppt",".pptx",".txt"]
+import os
+
+imagesExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "svg", "webp", ]
+videoExtensions = ["mp4", "mkv", "mov", "avi", "flv", "wmv"]
+docsExtensions = ["pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt"]
+
+
+def get_file_extension(file_path):
+    _, ext = os.path.splitext(file_path)
+    return ext.lower()
