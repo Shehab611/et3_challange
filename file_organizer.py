79da228d94d78c8be56e17007afc6502f8833fe2
Shehab Ehab <shehabeldenehab@gmail.com>
2025-08-12 19:36:35 +0300

    create a method that gets the category name

--- file_organizer.py
+++ file_organizer.py
@@ -8,3 +8,15 @@ docsExtensions = ["pdf", "doc", "docx", "xls", "xlsx", "ppt", "pptx", "txt"]
 def get_file_extension(file_path):
     _, ext = os.path.splitext(file_path)
     return ext.lower()
+
+
+def get_category_name(file_path):
+    ext = get_file_extension(file_path)
+    if ext in imagesExtensions:
+        return "images"
+    elif ext in videoExtensions:
+        return "videos"
+    elif ext in docsExtensions:
+        return "documents"
+    else:
+        return "others"
