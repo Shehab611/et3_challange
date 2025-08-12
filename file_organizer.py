87c6e5747251309323ab27a5919ff9b46cd2b9fe
Shehab Ehab <shehabeldenehab@gmail.com>
2025-08-12 19:27:51 +0300

    create the extensions of the necessary categories

---
+++ file_organizer.py
@@ -0,0 +1,3 @@
+imagesExtensions = [".jpg",".jpeg",".png", ".gif", ".bmp", ".tiff", ".svg", ".webp", ]
+videoExtensions = [".mp4",".mkv",".mov",".avi", ".flv",".wmv"]
+docsExtensions = [".pdf",".doc", ".docx",".xls", ".xlsx",".ppt",".pptx",".txt"]
